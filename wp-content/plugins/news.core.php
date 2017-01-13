9e644d97a7abd0d93ce33e3f07b791c6a222b0c8
thymochenko <henkosato5@gmail.com>
2017-01-13 18:05:23 -0300

    testando formulários

--- wp-content/plugins/news.core.php
+++ wp-content/plugins/news.core.php
@@ -143,9 +143,6 @@ class DaoNewslleter {
     }
     //@newslleter persist
     try {
-    
-      var_dump($data);
-
       $sth = Connection::open()->prepare("INSERT INTO wp_newslleter_contact
         (name,email,ip, msg, book_id, status,datecreated, dateupdated)
           VALUES (
@@ -164,10 +161,6 @@ class DaoNewslleter {
 
       $sth->bindValue(':datecreated', $dateTime->format('Y-m-d H:i:s') , PDO::PARAM_STR);
       $sth->bindValue(':dateupdated', $dateTime->format('Y-m-d H:i:s') , PDO::PARAM_STR);
-
-      var_dump($sth);
-
-      var_dump($sth->execute());exit;
 
       return ($sth->execute()) ? true : false;
 
