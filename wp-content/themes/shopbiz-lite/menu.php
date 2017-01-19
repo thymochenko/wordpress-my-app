f0b80f8c7a56a9f6cb5b8e5bdf0b610d8c7a506d
thymochenko <henkosato5@gmail.com>
2017-01-19 10:56:54 -0300

    task3: links: concluido mas com ressalvas e possíveis alterações no futuro

---
+++ wp-content/themes/shopbiz-lite/menu.php
@@ -0,0 +1,30 @@
+<div class="collapse navbar-collapse" id="navbar-wp">
+  <ul id="menu-main" class="nav navbar-nav navbar-right"><li id="menu-item-111" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-4 current_page_item menu-item-111 active"><a title="Home" href="<?php echo get_site_url();?>">Home</a></li>
+<li id="menu-item-110" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-110"><a title="About" href="<?php echo get_site_url(); ?>/sobre/">Sobre</a></li>
+<li id="menu-item-109" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-109"><a title="Services" href="<?php echo get_site_url(); ?>/blog/">Blog</a></li>
+<!--<li id="menu-item-216" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-216 dropdown"><a title="Portfolio" href="#">Portfolio <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+<li id="menu-item-215" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-215"><a title="Portfolio Two Column" href="https://themeansar.com/demo/wp/shopbiz/default/portfolio-two-column/">Portfolio Two Column</a></li>
+<li id="menu-item-214" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-214"><a title="Portfolio Three Column" href="https://themeansar.com/demo/wp/shopbiz/default/portfolio-three-column/">Portfolio Three Column</a></li>
+<li id="menu-item-213" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213"><a title="Portfolio Four Column" href="https://themeansar.com/demo/wp/shopbiz/default/portfolio-four-column/">Portfolio Four Column</a></li>
+</ul>
+</li>-->
+<li id="menu-item-108" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-108"><a title="Pricing" href="<?php echo get_site_url(); ?>/index.php/materiais-gratuitos/">Materiais Gratuitos</a></li>
+<!-- <li id="menu-item-183" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-183 dropdown"><a title="Blog">Blog <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+<li id="menu-item-186" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-186"><a title="Blog Full width" href="https://themeansar.com/demo/wp/shopbiz/default/blog-full-width/">Blog Full width</a></li>
+<li id="menu-item-127" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-127"><a title="Blog Left Sidebar" href="https://themeansar.com/demo/wp/shopbiz/default/blog-left-sidebar/">Blog Left Sidebar</a></li>
+<li id="menu-item-128" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-128"><a title="Blog Right Sidebar" href="https://themeansar.com/demo/wp/shopbiz/default/blog-right-sidebar/">Blog Right Sidebar</a></li>
+</ul>
+</li>-->
+<!-- <li id="menu-item-107" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-107 dropdown"><a title="Shop" href="https://themeansar.com/demo/wp/shopbiz/default/shop/">Shop <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+<li id="menu-item-182" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-182"><a title="Cart" href="https://themeansar.com/demo/wp/shopbiz/default/cart/">Cart</a></li>
+<li id="menu-item-181" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-181"><a title="Checkout" href="https://themeansar.com/demo/wp/shopbiz/default/checkout/">Checkout</a></li>
+<li id="menu-item-180" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-180"><a title="My Account" href="https://themeansar.com/demo/wp/shopbiz/default/my-account/">My Account</a></li>
+</ul>
+</li>-->
+<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130"><a title="Contact" href="<?php echo get_site_url(); ?>/consultoria/">Consultoria</a></li>
+<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130"><a title="Contact" href="<?php echo get_site_url(); ?>/cursos/">Cursos</a></li>
+<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130"><a title="Contact" href="<?php echo get_site_url(); ?>/contato/">Contato</a></li>
+</ul>        </div>
